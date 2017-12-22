59a44e2a6e4af76aeaf068968a4ba0a9e2c51afe
abhinav <itsabhinavrahul@gmail.com>
2017-12-22 19:17:06 +0530

    1st commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <p>
+      <h1 class="title"> Indus Ocean </h1>
+      <h2 class="subtitle"> hahahahha</h2> 
+      </p>
+    </div>
+  </section>
+</template>
