11cc3040116d78bf95eb92616dcf5df22b260f03
iamomprakashmehra <iamomprakashmehra@gmail.com>
2018-05-15 21:04:42 +0530

    update vue

--- pages/projects.vue
+++ pages/projects.vue
@@ -3,7 +3,7 @@
     <div class="container has-text-centered">
       <h1 class="title">My Projects</h1>
       <p>
-        Some of my projects can be found on 
+        Some of my projects can be found on YANDEX, a russian company.
       </p>
     </div>
   </section>
