63c6ffacfbcc4a1aa2781d90a14856d66d573953
iamomprakashmehra <iamomprakashmehra@gmail.com>
2018-05-15 21:18:28 +0530

    update eirheriu

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,7 +3,8 @@
     <div class="container has-text-centered">
       <h1 class="title">Contact Me</h1>
       <p>
-        You can follow me on Twitter:
+        You can follow me on Twitter: YANDEX
+        yaalh yaaahaaaa
       </p>
     </div>
   </section>
